5fe1c3ecf87d5a52b37afaa9427a912de31c31ac
Vladimir Levchuk <salyarka@gmail.com>
2018-02-07 21:57:08 +0300

    Added, registration new client socket for EPOLLIN event and disconnecting client.

--- server.py
+++ server.py
@@ -9,34 +9,34 @@ def shake_hands():
 
 s_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
 s_sock.bind(('127.0.0.1', 59599))
-# see man page for listen
-s_sock.listen(128)
+# by default is sets to socket.SOMAXCONN
+s_sock.listen()
 # set non-blocking mode
 s_sock.setblocking(0)
 ep = select.epoll()
 # register event for connection to server socket
-ep.register(ep.fileno(), select.EPOLLIN)
+ep.register(s_sock.fileno(), select.EPOLLIN)
+connections = {}
 
 while True:
-    events = ep.poll(1)
+    events = ep.poll()
     for fd, ev in events:
         if fd == s_sock.fileno():
             c_sock, c_addr = s_sock.accept()
             print('client connected, addr: %s' % (c_addr,))
+            c_sock.setblocking(0)
+            ep.register(c_sock.fileno(), select.EPOLLIN)
+            connections[c_sock.fileno()] = c_sock
+        elif ev and select.EPOLLHUP:
+            # client disonnected
+            print('client disconnected')
+            ep.unregister(fd)
+            connections[fd].close()
+            del connections[fd]
         elif ev and select.EPOLLIN:
             # data arrived from client
-            pass
+            print('reecived %s' % connections[fd].recv(1024))
         elif ev and select.EPOLLOUT:
             # it is able to send data to client
-            pass
-        elif event and select.EPOLLHUP:
-            # client disonnected
-            pass
-    #while True:
-    #    data = c_sock.recv(1024)
-    #    if not data:
-    #        print('client disconnected')
-    #        break
-    #    c_sock.sendall(b'echo: %s' % data)
-    #c_sock.close()
+            print('epollout')
 

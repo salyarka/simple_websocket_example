1cb064618f977a5cdfb096ea8fbb99c0972962f5
Vladimir Levchuk <salyarka@gmail.com>
2018-02-12 23:10:10 +0300

    Added, functions for decode and send frames.

--- server.py
+++ server.py
@@ -45,6 +45,41 @@ def close_conn(fd):
     conns[fd].close()
     del conns[fd]
 
+
+def decode_frame(frame):
+    opcode_and_fin = frame[0]
+
+    # assuming it's masked, hence removing the mask bit(MSB) to get len.
+    # also assuming len is <125
+    payload_len = frame[1] - 128
+
+    mask = frame[2:6]
+    encrypted_payload = frame[6: 6+payload_len]
+
+    payload = bytearray(
+        [
+            encrypted_payload[i] ^ mask[i%4]
+            for i in range(payload_len)
+        ]
+    )
+
+    return payload
+
+
+def send_frame(payload, conn):
+    # setting fin to 1 and opcpde to 0x1
+    frame = [129]
+    # adding len. no masking hence not doing +128
+    frame += [len(payload)]
+    # adding payload
+    frame_to_send = bytearray(frame) + payload
+
+    conn.send(frame_to_send)
+
+
+class Client:
+    pass
+
 # TODO: distinguish connection that are upgraded (make Client class???)
 try:
     while True:
